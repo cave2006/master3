142a79cf3f5b9938ef50cfba983c617ed74bc35e
Aleksey A. Morozov <AlekVolsk@users.noreply.github.com>
2018-10-15 20:04:57 +0400

    Add files via upload

--- html/layouts/com_contact/joomla/form/renderfield.php
+++ html/layouts/com_contact/joomla/form/renderfield.php
@@ -7,9 +7,12 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+defined( '_JEXEC' ) or die;
 
-extract($displayData);
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
+
+extract( $displayData );
 
 /**
  * Layout variables
@@ -19,14 +22,14 @@ extract($displayData);
  * 	$input           : (string) The input field html code
  */
 
-if (!empty($options['showonEnabled']))
+if ( !empty( $options[ 'showonEnabled' ] ) )
 {
-	JHtml::_('jquery.framework');
-	JHtml::_('script', 'jui/cms.js', array('version' => 'auto', 'relative' => true));
+	HTMLHelper::_( 'jquery.framework' );
+	HTMLHelper::_( 'script', 'jui/cms.js', [ 'version' => 'auto', 'relative' => true ] );
 }
 
-$class = empty($options['class']) ? '' : ' ' . $options['class'];
-$rel   = empty($options['rel']) ? '' : ' ' . $options['rel'];
+$class = empty( $options[ 'class' ] ) ? '' : ' ' . $options[ 'class' ];
+$rel   = empty( $options[ 'rel' ] ) ? '' : ' ' . $options[ 'rel' ];
 
 /**
  * @TODO:
@@ -38,20 +41,23 @@ $rel   = empty($options['rel']) ? '' : ' ' . $options['rel'];
  * On a side note: Parsing html is seldom a good idea.
  * https://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags/1732454#1732454
  */
-preg_match('/class=\"([^\"]+)\"/i', $input, $match);
+preg_match( '/class=\"([^\"]+)\"/i', $input, $match );
 
-$required      = (strpos($input, 'aria-required="true"') !== false || (!empty($match[1]) && strpos($match[1], 'required') !== false));
-$typeOfSpacer  = (strpos($label, 'spacer-lbl') !== false);
+$required      = ( strpos( $input, 'aria-required="true"' ) !== false || ( !empty( $match[ 1 ] ) && strpos( $match[ 1 ], 'required' ) !== false ) );
+$typeOfSpacer  = ( strpos( $label, 'spacer-lbl') !== false );
 
 ?>
-<div class="control-group<?php echo $class; ?>"<?php echo $rel; ?>>
-	<?php if (empty($options['hiddenLabel'])) : ?>
-		<div class="control-label">
-			<?php echo $label; ?>
-			<?php if (!$required && !$typeOfSpacer) : ?>
-				<span class="optional"><?php echo JText::_('COM_CONTACT_OPTIONAL'); ?></span>
-			<?php endif; ?>
-		</div>
-	<?php endif; ?>
-	<div class="controls"><?php echo $input; ?></div>
+<div class="uk-form-stacked uk-margin <?php echo $class; ?>"<?php echo $rel; ?>>
+	<?php if ( empty( $options[ 'hiddenLabel' ] ) ) { ?>
+	<div class="uk-form-label">
+		<?php
+		echo $label;
+		if ( !$required && !$typeOfSpacer )
+		{
+		?>
+		<span class="uk-text-danger"><?php echo Text::_( 'COM_CONTACT_OPTIONAL' ); ?></span>
+		<?php } ?>
+	</div>
+	<?php } ?>
+	<div class="uk-form-controls"><?php echo $input; ?></div>
 </div>
