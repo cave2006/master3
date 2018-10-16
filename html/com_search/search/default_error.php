1d30303490b08aa254350431598cbcc43761d41f
Aleksey A. Morozov <AlekVolsk@users.noreply.github.com>
2018-10-17 02:47:59 +0400

    Add files via upload

---
+++ html/com_search/search/default_error.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_search
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+if ($this->error)
+{
+?>
+<div class="uk-alert uk-alert-danger">
+	<?php echo $this->escape($this->error); ?>
+</div>
+<?php
+}
